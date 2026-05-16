ec2a0d0df36003402baffe4fe2eec7f28bd29a6a
CuervoOFC <Ismaelsolis151@gmail.com>
2026-05-16 04:37:10 -0600

    Add NSFW video command for group usage

---
+++ src/commands/all/nsfw/video.ts
@@ -0,0 +1,104 @@
+/**
+ * @author Hiudy · github.com/hiudyy
+ * @project Misa Bot
+ */
+
+import axios from "axios";
+import { WAMessage } from "baileys";
+import { Command } from "../../../types/Command.js";
+import { getGroup } from "../../../database/groupDB.js";
+
+const videoCommand: Command = {
+  name: "pornvid",
+  aliases: ["nsfwvid"],
+  description: "Envia videos NSFW",
+  category: "nsfw",
+  groupOnly: true,
+
+  async execute({ misa, message, from }) {
+    try {
+      // =========================
+      // VERIFICAR NSFW
+      // =========================
+      const group = await getGroup(from);
+
+      if (!group.nsfw.ativo) {
+        await misa.sendMessage(
+          from,
+          {
+            text:
+              "❌ O sistema NSFW está desativado neste grupo.",
+          },
+          { quoted: message as WAMessage },
+        );
+
+        return;
+      }
+
+      // =========================
+      // API
+      // =========================
+      const { data } = await axios.get(
+        "https://api.evogb.org/porn/video/straight?key=evogb-WzR3kPpa",
+      );
+
+      if (!data.status) {
+        await misa.sendMessage(
+          from,
+          {
+            text:
+              "❌ Não foi possível obter o video.",
+          },
+          { quoted: message as WAMessage },
+        );
+
+        return;
+      }
+
+      // =========================
+      // PEGAR IMAGEM
+      // =========================
+      const image =
+        data.data?.url ||
+        data.data ||
+        data.url;
+
+      if (!image) {
+        await misa.sendMessage(
+          from,
+          {
+            text:
+              "❌ A API não retornou nenhum video.",
+          },
+          { quoted: message as WAMessage },
+        );
+
+        return;
+      }
+
+      // =========================
+      // ENVIAR IMAGEM
+      // =========================
+      await misa.sendMessage(
+        from,
+        {
+          video: { url: image },
+          caption: "🔞 NSFW",
+        },
+        { quoted: message as WAMessage },
+      );
+    } catch (error) {
+      console.error(error);
+
+      await misa.sendMessage(
+        from,
+        {
+          text: `❌ Ocorreu um erro.\n\n${String(error)}`,
+        },
+        { quoted: message as WAMessage },
+      );
+    }
+  },
+};
+
+export default videoCommand;
